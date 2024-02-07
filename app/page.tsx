a0e69f027400af95f76ecf40bfab8dfe27fdabcd
SantiZai <santyagozaidan@gmail.com>
2024-02-07 17:53:06 -0300

    adding date picker for select day for the turn

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Presentation from "@/components/Presentation";
-import SearchBento from "@/components/SearchBento";
+import SearchCourts from "@/components/SearchCourt";
 
 const Home = () => {
   return (
     <main className="px-2 py-4">
       <section className="grid grid-cols-1 md:grid-cols-10 md:grid-rows-1">
           <Presentation class="md:col-span-4" />
-          <SearchBento class="md:col-span-6" />
+          <SearchCourts class="md:col-span-6" />
       </section>
     </main>
   );
---
+++ components/SearchCourt.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import { cn } from "@/lib/utils";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
+import { Calendar } from "./ui/calendar";
+import { es } from "date-fns/locale";
+
+const SearchCourts = (props: {class: string}) => {
+  const [date, setDate] = useState<Date>();
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className={props.class}>
+      <fieldset className="w-[200px]">
+        <Label htmlFor="location">Ubicación</Label>
+        <Input
+          id="location"
+          type="text"
+          placeholder="Buscar ciudad"
+        />
+      </fieldset>
+      <fieldset className="w-[200px]">
+        <Label>Elige el deporte</Label>
+        <Select>
+          <SelectTrigger>
+            <SelectValue placeholder="Deporte" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="tenis">Tenis</SelectItem>
+            <SelectItem value="futbol">Fútbol</SelectItem>
+          </SelectContent>
+        </Select>
+      </fieldset>
+      <fieldset className="w-[200px]">
+        <Label>Selecciona el día</Label>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant={"outline"} className={cn("w-full justify-start text-left font-normal px-3 py-2", !date && "text-muted-foreground")}>
+              {date ? format(date, "eee d 'de' MMMM 'de' y", {locale: es}) : <span>Selecciona una fecha</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar mode="single" selected={date} onSelect={setDate} />
+          </PopoverContent>
+        </Popover>
+      </fieldset>
+      <fieldset className="w-[200px]">
+        <Label htmlFor="location">Ubicación</Label>
+        <Input
+          id="location"
+          type="text"
+          placeholder="Buscar ciudad"
+        />
+      </fieldset>
+      <Button type="submit">Buscar</Button>
+    </form>
+  );
+};
+
+export default SearchCourts;
