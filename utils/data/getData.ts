13e2b11ef46be00ee79e94f10630342c3bb48e8e
SantiZai <santyagozaidan@gmail.com>
2024-02-27 13:45:17 -0300

    get filtered clubs

--- utils/data/getData.ts
+++ utils/data/getData.ts
@@ -1,4 +1,16 @@
+const API_BASE = "http://localhost:5000/";
+
 export const getClubs = async () => {
-  const result = await fetch("http://localhost:4000/clubs");
+  const result = await fetch(`${API_BASE}clubs`);
+  return result.json();
+};
+
+export const getFilteredClubs = async (queries: {
+  location: string;
+  sport: string;
+}) => {
+  const result = await fetch(
+    `${API_BASE}clubs/search/many?location=${queries.location}&sport=${queries.sport}`
+  );
   return result.json();
 };
--- utils/data/models.ts
+++ utils/data/models.ts
@@ -1,9 +1,7 @@
-interface Club {
+export interface Club {
   id: string;
   name: string;
   location: string;
   createdAt: string;
   adminId: string;
 }
-
-export type { Club };
