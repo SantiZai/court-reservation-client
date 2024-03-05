04862307de57f7bb67332c32c46457601c9e5cac
SantiZai <santyagozaidan@gmail.com>
2024-03-05 12:17:38 -0300

    add models for autocomplete

--- utils/data/models.ts
+++ utils/data/models.ts
@@ -1,7 +1,36 @@
+export enum SPORTS {
+  tenis,
+  futbol,
+  basquet,
+  voleibol,
+  hockey,
+  rugby,
+}
+
+export enum SURFACES {
+  polvo_de_ladrillo,
+  cemento,
+  cesped,
+  parquet,
+  arena,
+  alfombra,
+}
+
 export interface Club {
   id: string;
+  createdAt: string;
   name: string;
   location: string;
-  createdAt: string;
   adminId: string;
+  courts: Court[];
+  sports: SPORTS[];
+}
+
+export interface Court {
+  id: string;
+  createdAt: string;
+  name: string;
+  surface: SURFACES;
+  clubId: string;
+  sport: SPORTS;
 }
