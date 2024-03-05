edef5796da92815948541bc10ec95771bd5592b8
SantiZai <santyagozaidan@gmail.com>
2024-03-05 12:16:57 -0300

    display the selected club

--- app/search/details/[id]/page.tsx
+++ app/search/details/[id]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import HeaderMobile from "@/components/HeaderMobile";
+import HeaderWeb from "@/components/HeaderWeb";
 import { getClub } from "@/utils/data/getData";
-import { Club } from "@/utils/data/models";
+import { Club, Court } from "@/utils/data/models";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -10,13 +12,35 @@ const DetailsPage = () => {
   const params = useParams();
 
   useEffect(() => {
-    params.id && getClub(params.id as string).then((club: Club) => setClub(club));
+    params.id &&
+      getClub(params.id as string).then((club: Club) => setClub(club));
   }, [params.id]);
 
   return (
-    <div>
-      <section>{club && <h3>{club.name}</h3>}</section>
-    </div>
+    <main>
+      <HeaderWeb>
+        <section>web</section>
+      </HeaderWeb>
+      <HeaderMobile class="w-full pt-4 border-b-2">
+        <section className="w-full flex items-center gap-4 p-4">logo</section>
+      </HeaderMobile>
+      {club && (
+        <section className="h-full w-full bg-slate-200">
+          <article className="h-[40%] w-full p-4">photo</article>
+          <article className="h-[60%] w-full p-4 bg-white rounded-t-3xl">
+            <h3>{club.name}</h3>
+            <h4>{club.location}</h4>
+            {club.courts.map((court: Court) => {
+              return (
+                <div key={court.id}>
+                  <p>{court.name}</p>
+                </div>
+              );
+            })}
+          </article>
+        </section>
+      )}
+    </main>
   );
 };
 
