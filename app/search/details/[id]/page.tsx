0198a8894638eef5b70a1cbeae0b8f1eca2f2f65
SantiZai <santyagozaidan@gmail.com>
2024-03-04 23:55:34 -0300

    detail fixed

--- app/search/details/[id]/page.tsx
+++ app/search/details/[id]/page.tsx
@@ -1,18 +1,21 @@
-"use client"
+"use client";
 
+import { getClub } from "@/utils/data/getData";
+import { Club } from "@/utils/data/models";
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const DetailsPage = () => {
+  const [club, setClub] = useState<Club>();
   const params = useParams();
 
   useEffect(() => {
-    
-  }, [params.id])
+    params.id && getClub(params.id as string).then((club: Club) => setClub(club));
+  }, [params.id]);
 
   return (
     <div>
-      <section>details page</section>
+      <section>{club && <h3>{club.name}</h3>}</section>
     </div>
   );
 };
